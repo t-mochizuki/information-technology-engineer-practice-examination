00bd94c62c6d2122bc41a9caee98403f488bf5d8
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-10-21 22:56:32 +0900

    カスタム要素を解答欄用から解答用紙用に変更する

--- docs/main.js
+++ docs/main.js
@@ -1,34 +1,51 @@
 (() => {
-  class AnswerFieldSetElement extends HTMLFieldSetElement {
+  function createAnswerFieldSetElement(href, nth) {
+    const fieldsetElem = document.createElement("fieldset");
+
+    const aElem = document.createElement("a");
+    aElem.setAttribute("href", href);
+    aElem.innerText = `問${nth}`;
+
+    const legendElem = document.createElement("legend");
+    legendElem.appendChild(aElem);
+    fieldsetElem.appendChild(legendElem);
+
+    [
+      "ア",
+      "イ",
+      "ウ",
+      "エ"
+    ].forEach((text, index) => {
+      const labelElem = document.createElement("label");
+      const inputElem = document.createElement("input");
+      inputElem.setAttribute("type", "radio");
+      inputElem.setAttribute("name", `question${nth}`);
+      inputElem.setAttribute("value", index + 1);
+      labelElem.appendChild(inputElem);
+      labelElem.appendChild(new Text(text));
+      fieldsetElem.appendChild(labelElem);
+    });
+
+    return fieldsetElem;
+  }
+
+  class AnswerFormElement extends HTMLFormElement {
     constructor() {
       super();
 
-      const aElem = document.createElement("a");
-      aElem.setAttribute("href", this.dataset.href);
-      aElem.innerText = `問${this.dataset.index}`;
-
-      const legendElem = document.createElement("legend");
-      legendElem.appendChild(aElem);
-      this.appendChild(legendElem);
-
-      [
-        "ア",
-        "イ",
-        "ウ",
-        "エ"
-      ].forEach((text, index) => {
-        const labelElem = document.createElement("label");
-        const inputElem = document.createElement("input");
-        inputElem.setAttribute("type", "radio");
-        inputElem.setAttribute("name", `question${this.dataset.index}`);
-        inputElem.setAttribute("value", index + 1);
-        labelElem.appendChild(inputElem);
-        labelElem.appendChild(new Text(text));
-        this.appendChild(labelElem);
-      });
+      const href = this.dataset.href;
+      for (let i = 1; i <= this.dataset.total; i++) {
+        const fieldsetElem = createAnswerFieldSetElement(`${href}${i}/index.html`, i);
+        this.appendChild(fieldsetElem);
+      }
+      const inputElem = document.createElement("input");
+      inputElem.setAttribute("type", "submit");
+      inputElem.setAttribute("value", "Submit");
+      this.appendChild(inputElem);
+      this.appendChild(document.createElement("output"));
     }
   }
-  customElements.define("answer-fieldset", AnswerFieldSetElement, { extends: "fieldset" });
+  customElements.define("answer-form", AnswerFormElement, { extends: "form" });
 
   const formElem = document.querySelector("form");
 
