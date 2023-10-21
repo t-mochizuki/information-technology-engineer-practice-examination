cea0efd331ab5927ab9c7ca4372df9b67c4d31ff
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-10-21 18:16:26 +0900

    提出ボタンを押した時に解答用紙を採点する

---
+++ docs/main.js
@@ -0,0 +1,27 @@
+(() => {
+  const formElem = document.querySelector("form");
+
+  formElem.onsubmit = (e) => {
+    e.preventDefault();
+
+    new FormData(formElem);
+  };
+
+  const answers = {
+    "question1": "1",
+    "question2": "1"
+  };
+
+  const outputElem = document.querySelector("output");
+
+  formElem.onformdata = (e) => {
+    const formData = e.formData;
+    let count = 0;
+    formData.forEach((answer, index) => {
+      if (answer === answers[index]) {
+        count++;
+      }
+    })
+    outputElem.value = `Your score: ${count} / 2`;
+  }
+})();
