23f2687b374045e225c6d29a7627ebf2ce5aa219
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-10-21 15:26:37 +0900

    令和5年度 秋期 午前Ⅰ 問2を追加する

--- docs/main.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  const body = document.querySelector("body");
-  fetch("questions/2023/autumn/am1/index.html")
-    .then((response) => response.text())
-    .then((data) => (body.innerHTML = data));
-})();
