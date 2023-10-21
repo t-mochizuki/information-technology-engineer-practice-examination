78ba8789f842de2af348f7fcaaaf7da844eb795b
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-10-21 22:26:41 +0900

    解答欄用のカスタム要素を追加する

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,35 @@
 (() => {
+  class AnswerFieldSetElement extends HTMLFieldSetElement {
+    constructor() {
+      super();
+
+      const aElem = document.createElement("a");
+      aElem.setAttribute("href", this.dataset.href);
+      aElem.innerText = `問${this.dataset.index}`;
+
+      const legendElem = document.createElement("legend");
+      legendElem.appendChild(aElem);
+      this.appendChild(legendElem);
+
+      [
+        "ア",
+        "イ",
+        "ウ",
+        "エ"
+      ].forEach((text, index) => {
+        const labelElem = document.createElement("label");
+        const inputElem = document.createElement("input");
+        inputElem.setAttribute("type", "radio");
+        inputElem.setAttribute("name", `question${this.dataset.index}`);
+        inputElem.setAttribute("value", index + 1);
+        labelElem.appendChild(inputElem);
+        labelElem.appendChild(new Text(text));
+        this.appendChild(labelElem);
+      });
+    }
+  }
+  customElements.define("answer-fieldset", AnswerFieldSetElement, { extends: "fieldset" });
+
   const formElem = document.querySelector("form");
 
   formElem.onsubmit = (e) => {
